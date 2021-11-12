77536486cdfadbe7a796f024c2c2cf1eca931ba6
hkanjohansson <hj222je@student.lnu.se>
2021-11-12 16:55:49 +0100

    Moved all functions into memory

--- memory/src/components/memory.js
+++ memory/src/components/memory.js
@@ -1,51 +1,74 @@
-import Tile from './tile'
 import { useState, useEffect } from 'react'
+import './tile.css'
 
 /*
-  *Jag vill på något sätt jämföra två brickor om de två är lika.
-    -
+Create global states for tile1_flipped and tile2_flipped
 
+Check id of tile flipped and set flipped
 */
-function GameLogic({ flippedNumber, tile_1, tile_2 }) {
-  const [countFlipped, setCountFlipped] = useState(0)
-  let tiles_equal = false
 
-  console.log(`gameLogic ${flippedNumber}`)
-  useEffect(() => {
-    console.log('Hej')
-    tiles_equal = tile_1 === tile_2
-  }, [flippedNumber])
-
+function Image() {
   return (
-    true
+    <img className='image' src={'./logo192.png'} />
   )
 }
 
-function Memory() {
-  const [flippedNumber, setFlippedNumber] = useState([1, 2, 3, 4, 5, 6])
-
+function Tile({ flippedNumber }) {
+  const image = <Image />
+  const [tileValue, setTileValue] = useState(image)
+  const [flipped, setFlipped] = useState(false)
+  
   /*
-    Här vill jag lyssna på flip events och definiera vilken som är tile_1 och tile_2
-    för att sedan skicka som props till GameLogic?
+    useEffect to change the global state
 
-    Skulle useEffect kunna användas här?
+    Dependent on flipped and id of that tile
   */
+
+  const handleClick = () => {
+    if (!flipped) {
+      setTileValue(flippedNumber)
+      setFlipped(true)
+    } else {
+      setTileValue(image)
+      setFlipped(false)
+    }
+  }
+
+  return (
+    <div className='tile' onClick={handleClick}>{tileValue}</div>
+  )
+}
+
+function Memory({ tile1, tile2 }) {
+  const [flippedNumber, setFlippedNumber] = useState([1, 2, 3, 4, 5, 6])
+  const [flippedTile1, setFlippedTile1] = useState(false)
+  const [flippedTile2, setFlippedTile2] = useState(false)
+  // Define a variable to keep track of number of tiles flipped ----> n === #tiles: Game over
+  
+  useEffect(() => {
+    if (flippedTile1 && flippedTile2) {
+      // Check if match and handle if flipping back or not
+    }
+
+  }, [flippedTile1, flippedTile2])
+
   return (
     <div className='grid-container'>
-      <GameLogic flippedNumber={flippedNumber} tile_1={tile_1} tile_2={tile_2} /> 
-      <Tile flippedNumber={flippedNumber[0]} />
-      <Tile flippedNumber={flippedNumber[0]} />
-      <Tile flippedNumber={flippedNumber[1]} />
-      <Tile flippedNumber={flippedNumber[1]} />
-      <Tile flippedNumber={flippedNumber[2]} />
-      <Tile flippedNumber={flippedNumber[2]} />
+      <Tile id={1} flippedNumber={flippedNumber[0]} />
+      <Tile id={2} flippedNumber={flippedNumber[0]} />
+      <Tile id={3} flippedNumber={flippedNumber[1]} />
+      
+      <Tile id={4} flippedNumber={flippedNumber[1]} />
+      <Tile id={5} flippedNumber={flippedNumber[2]} />
+      <Tile id={6} flippedNumber={flippedNumber[2]} />
 
-      <Tile flippedNumber={flippedNumber[3]} />
-      <Tile flippedNumber={flippedNumber[3]} />
-      <Tile flippedNumber={flippedNumber[4]} />
-      <Tile flippedNumber={flippedNumber[4]} />
-      <Tile flippedNumber={flippedNumber[5]} />
-      <Tile flippedNumber={flippedNumber[5]} />
+      <Tile id={7} flippedNumber={flippedNumber[3]} />
+      <Tile id={8} flippedNumber={flippedNumber[3]} />
+      <Tile id={9} flippedNumber={flippedNumber[4]} />
+      
+      <Tile id={10} flippedNumber={flippedNumber[4]} />
+      <Tile id={11} flippedNumber={flippedNumber[5]} />
+      <Tile id={12} flippedNumber={flippedNumber[5]} />
     </div>
   )
 }
