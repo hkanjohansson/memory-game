49d47fd80418d832835450a0e40671f86928d5d5
hkanjohansson <hj222je@student.lnu.se>
2021-11-18 18:28:51 +0100

    Trying out useContext

--- memory/src/components/memory.js
+++ memory/src/components/memory.js
@@ -1,18 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react'
 import './tile.css'
+import { MemoryContext } from '../index'
 
 /*
 Create global states for tile1_flipped and tile2_flipped
 
 Check id of tile flipped and set flipped
 */
-const flippedCount = React.createContext(0)
-const flipped = React.createContext(false)
 
-const flippedTiles = React.createContext({ 
-  first: null,
-  second: null
-})
 
 function Image() {
   return (
@@ -23,58 +18,59 @@ function Image() {
 function Tile({ id, flippedNumber }) {
   const image = <Image />
   const [tileValue, setTileValue] = useState(image)
-  const [flippedTile, setFlipped] = useState(flipped)
-  //const [flippedTile] = flipped.Provider
-  const [flippedCounter, setFlippedCounter] = useState(useContext(flippedCount))
-  
-  /*
-    useEffect to change the global state
+  const [flipped, setFlipped] = useState(false)
 
-    Dependent on flipped and id of that tile
+  /*
+    #######
+    Ska jag konsumera context här? Och sen i Memory?
+    #######
   */
+  const [memoryState, setMemoryState] = useState(useContext(MemoryContext))
 
   const handleClick = () => {
-    if (!flippedTile) {
+    if (!flipped) {
       setTileValue(flippedNumber)
-      
-      //flippedTile = true
       setFlipped(true)
-      setFlippedCounter(prev => prev + 1)
-
+      /*
+      ##############
+        Denna sätter jag för att se om jag får en reaktion i useEffect i Memory-komponenten, men 
+        det funkar inte än.
+      ##############
+      */
+      setMemoryState({
+        flippedCount: 1, // ##### Här vill jag öka flippedCount och sen kolla i Memory om det är lika med antalet brickor, jämföra om två nya är flippade etc. 
+        flippedTiles: { first: id, second: null }}) 
     } else {
       setTileValue(image)
-      //flippedTile = false
       setFlipped(false)
+      setMemoryState({
+        flippedCount: 1,
+        flippedTiles: { first: null, second: null }}) 
     }
   }
 
-  useEffect(() => {
-    //setFlippedCounter(prev => prev + 1)
-    
-    console.log(flippedCounter)
-  }, [flippedCounter])
+  //console.log(memoryState)
 
   return (
     <div className='tile' onClick={handleClick}>{tileValue}</div>
   )
 }
 
-function Memory({ tile1, tile2 }) {
+function Memory() {
   const [flippedNumber, setFlippedNumber] = useState([1, 2, 3, 4, 5, 6])
-  console.log(`From memory component ${useContext(flippedCount)}`)
+  const [memoryState, setMemoryState] = useState(useContext(MemoryContext))
 
-  // Define a variable to keep track of number of tiles flipped ----> n === #tiles: Game over
   useEffect(() => {
-    console.log(`Does useContext work in Memory?`)
-    
-  }, [useContext(flippedCount)])
+    //setFlippedCount(prev => prev + 1)
+    console.log(`Hej hej från useEffect ${memoryState}`)
+  }, [memoryState.flippedCount])
 
   return (
     <div className='grid-container'>
       <Tile id={0} flippedNumber={flippedNumber[0]} />
       <Tile id={1} flippedNumber={flippedNumber[0]} />
       <Tile id={2} flippedNumber={flippedNumber[1]} />
-      
+
       <Tile id={3} flippedNumber={flippedNumber[1]} />
       <Tile id={4} flippedNumber={flippedNumber[2]} />
       <Tile id={5} flippedNumber={flippedNumber[2]} />
@@ -82,10 +78,11 @@ function Memory({ tile1, tile2 }) {
       <Tile id={6} flippedNumber={flippedNumber[3]} />
       <Tile id={7} flippedNumber={flippedNumber[3]} />
       <Tile id={8} flippedNumber={flippedNumber[4]} />
-      
+
       <Tile id={9} flippedNumber={flippedNumber[4]} />
       <Tile id={10} flippedNumber={flippedNumber[5]} />
       <Tile id={11} flippedNumber={flippedNumber[5]} />
+      <div className={'score-board'}>{memoryState.flippedCount}</div>
     </div>
   )
 }
--- memory/src/index.js
+++ memory/src/index.js
@@ -1,5 +1,22 @@
+import React from 'react'
 import ReactDOM from 'react-dom'
 import './components/tile.css'
 import Memory from './components/memory'
 
-ReactDOM.render(<Memory />, document.getElementById('root'))
+export const MemoryContext = React.createContext()
+
+function Main() {
+
+  const globalStates = {
+    flippedCount: 0,
+    flippedTiles: { first: null, second: null }
+  }
+
+  return (
+    <MemoryContext.Provider value={globalStates} >
+      <Memory />
+    </MemoryContext.Provider>
+  )
+}
+
+ReactDOM.render(<Main />, document.getElementById('root'))
